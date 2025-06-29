6a81568b99c0fcea3da4d2e78a2609c7ab18580c
Jairo Alessandro <r1067776@student.thomasmore.be>
2025-06-29 15:37:28 +0200

    last version

--- 2_Selection/extra_exercise1.py
+++ 2_Selection/extra_exercise1.py
@@ -1,8 +1,12 @@
 year = int(input("Enter a year: "))
 
-if year < 1582 and year % 4 == 0:
-    print("This is a leap year")
-elif year > 1582 and (year % 400 == 0 or year % 4 == 0):
-    print("This is a leap year")
+if year < 1582:
+    if year % 4 == 0:
+        print("This is a leap year")
+    else:
+        print("This is not a leap year")
 else:
-    print("This is not a leap year")
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print("This is a leap year")
+    else:
+        print("This is not a leap year")
